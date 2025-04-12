04bfd9f2eab2baed8b7cd6685767f1bef40a434e
danwritecode <dnelson4993@outlook.com>
2025-04-12 13:02:36 -0400

    server done?

--- common/src/types.rs
+++ common/src/types.rs
@@ -1,24 +1,36 @@
+use std::collections::HashMap;
+
 use crate::animation::{AnimationType, CharacterType, Facing};
 use bincode::{Decode, Encode};
 
+pub type ClientId = u64;
+pub type UserNameText = String;
 
-pub enum ClientServerEventType {
-    NewClient(ClientServerEventNewClient),
-    ClientCharacterUpdate(ClientServerEvent),
+/// This is what gets sent to the server AND the client.
+///
+/// When clients connect to the server, they send a ClientEventNewClient.
+///
+/// The server then sends NewClient events to all clients.
+///
+/// When a client updates their character state, they send a ClientServerEvent.
+///
+/// The server then sends ClientServerEvent to all clients.
+#[derive(Encode, Decode, PartialEq, Debug)]
+pub enum ClientEventType {
+    NewClient(HashMap<UserNameText, ClientId>),
+    ClientCharacterUpdate(HashMap<ClientId, ClientServerEvent>),
 }
 
-#[derive(Encode, Decode, PartialEq, Debug)]
-pub struct ClientServerEventNewClient {
-    pub client_id: u64,
-    x_pos: f32,
-    y_pos: f32,
-}
-
-#[derive(Encode, Decode, PartialEq, Debug)]
+#[derive(Encode, Decode, PartialEq, Debug, Clone)]
 pub struct ClientServerEvent {
+    /// The server gives us the client_id
     pub client_id: u64,
-    x_pos: f32,
-    y_pos: f32,
+
+    /// We give this to the server
+    pub username: String,
+
+    pub x_pos: f32,
+    pub y_pos: f32,
     pub facing: Facing,
 
     // this determines the texture
@@ -39,33 +51,9 @@ pub struct ClientServerEvent {
     pub sequence_frame_index: usize,
 }
 
-impl ClientServerEvent {
-    pub fn new(
-        client_id: u64,
-        x_pos: f32,
-        y_pos: f32,
-        facing: Facing,
-        anim_type: AnimationType,
-        character_type: CharacterType,
-        sprite_frame: usize,
-        sequence_index: usize,
-        sequence_frame_index: usize,
-    ) -> Self {
-        Self {
-            client_id,
-            x_pos,
-            y_pos,
-            facing,
-            anim_type,
-            character_type,
-            sprite_frame,
-            sequence_index,
-            sequence_frame_index,
-        }
-    }
-}
 
-
+/// ClientState is used to get the current generic state of a character
+/// We then convert that to a ClientServerEvent to send to the server
 #[derive(Encode, Decode, PartialEq, Debug)]
 pub struct ClientState {
     x_pos: f32,
@@ -110,6 +98,21 @@ impl ClientState {
             sprite_frame,
             sequence_index,
             sequence_frame_index,
+        }
+    }
+
+    fn into_client_server_event(self, client_id: u64, username: String) -> ClientServerEvent {
+        ClientServerEvent {
+            client_id,
+            username,
+            x_pos: self.x_pos,
+            y_pos: self.y_pos,
+            facing: self.facing,
+            anim_type: self.anim_type,
+            character_type: self.character_type,
+            sprite_frame: self.sprite_frame,
+            sequence_index: self.sequence_index,
+            sequence_frame_index: self.sequence_frame_index,
         }
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,11 @@
 use std::{
-    collections::HashSet, net::{SocketAddr, UdpSocket}, thread, time::{Duration, Instant, SystemTime}
+    collections::{HashMap, HashSet}, net::{SocketAddr, UdpSocket}, thread, time::{Duration, Instant, SystemTime}
 };
 
+use common::types::{ClientEventType, ClientId, UserNameText, ClientServerEvent};
 use renet::{ConnectionConfig, DefaultChannel, RenetServer, ServerEvent};
 use renet_netcode::{
-    NetcodeServerTransport, ServerAuthentication, ServerConfig,
+    NetcodeServerTransport, ServerAuthentication, ServerConfig, NETCODE_USER_DATA_BYTES,
 };
 
 
@@ -12,13 +13,6 @@ use renet_netcode::{
 // full exmaple here
 // https://github.com/lucaspoffo/renet/blob/master/renet/examples/echo.rs
 //
-
-
-// Client joins server
-// Server adds Character to Server's list of Characters
-// Server sends all Characters to all clients
-// Clients update their Characters
-
 
 fn main() {
     let server_addr: SocketAddr = format!("0.0.0.0:{}", 5000).parse().unwrap();
@@ -28,6 +22,9 @@ fn main() {
 const PROTOCOL_ID: u64 = 7;
 
 fn server(public_addr: SocketAddr) {
+    // bincode config
+    let config = bincode::config::standard();
+
     let connection_config = ConnectionConfig::default();
     let mut server: RenetServer = RenetServer::new(connection_config);
 
@@ -43,8 +40,10 @@ fn server(public_addr: SocketAddr) {
 
     let mut transport = NetcodeServerTransport::new(server_config, socket).unwrap();
 
-    let mut clients: HashSet<u64> = HashSet::new();
-    let mut received_messages: Vec<String> = vec![];
+    let mut clients: HashSet<ClientId> = HashSet::new();
+    let mut client_states: HashMap<ClientId, ClientServerEvent> = HashMap::new();
+    let mut client_mapping: HashMap<UserNameText, ClientId> = HashMap::new();
+
     let mut last_updated = Instant::now();
 
     loop {
@@ -55,20 +54,25 @@ fn server(public_addr: SocketAddr) {
         server.update(duration);
         transport.update(duration, &mut server).unwrap();
 
-        received_messages.clear();
-
         while let Some(event) = server.get_event() {
             match event {
                 ServerEvent::ClientConnected { client_id } => {
                     clients.insert(client_id);
 
+                    let user_data = transport.user_data(client_id).unwrap();
+                    let username = Username::from_user_data(&user_data);
+                    client_mapping.insert(username.0.clone(), client_id);
+
+                    let client_mapping_event = ClientEventType::NewClient(client_mapping.clone());
+                    let encoded_client_mapping_event = bincode::encode_to_vec(&client_mapping_event, config).unwrap();
+
                     server.broadcast_message_except(
-                        client_id,
-                        DefaultChannel::ReliableOrdered,
-                        format!("User \"{}\" connected", client_id),
+                        client_id, 
+                        DefaultChannel::ReliableOrdered, 
+                        encoded_client_mapping_event
                     );
 
-                    println!("Client {} connected.", client_id)
+                    println!("Client {} connected. Username: {}", client_id, username.0);
                 }
                 ServerEvent::ClientDisconnected { client_id, reason } => {
                     println!("Client {} disconnected: {}", client_id, reason);
@@ -76,20 +80,40 @@ fn server(public_addr: SocketAddr) {
             }
         }
 
+        // this is where we get client updates
         for client_id in server.clients_id() {
             while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered) {
-                // this is where we get client updates
-                // and we have to push them to all other clients
-                // create a vec to hold all messages
+                let (decoded, _len): (ClientServerEvent, usize) = bincode::decode_from_slice(&message[..], config).unwrap();
+                let client_id = decoded.client_id;
+
+                // insert or update client states
+                client_states.entry(client_id)
+                    .and_modify(|v| *v = decoded.clone())
+                    .or_insert(decoded);
             }
         }
 
-        for text in received_messages.iter() {
-            // make this a broadcast message except
-            server.broadcast_message(DefaultChannel::ReliableOrdered, text.as_bytes().to_vec());
-        }
+        // for each iteration of the loop, we send the client_states to all clients
+        let client_mapping_event = ClientEventType::ClientCharacterUpdate(client_states.clone());
+        let encoded_client_mapping_event = bincode::encode_to_vec(&client_mapping_event, config).unwrap();
+        server.broadcast_message(DefaultChannel::ReliableOrdered, encoded_client_mapping_event);
 
         transport.send_packets(&mut server);
         thread::sleep(Duration::from_millis(50));
+    }
+}
+
+
+struct Username(String);
+
+impl Username {
+    fn from_user_data(user_data: &[u8; NETCODE_USER_DATA_BYTES]) -> Self {
+        let mut buffer = [0u8; 8];
+        buffer.copy_from_slice(&user_data[0..8]);
+        let mut len = u64::from_le_bytes(buffer) as usize;
+        len = len.min(NETCODE_USER_DATA_BYTES - 8);
+        let data = user_data[8..len + 8].to_vec();
+        let username = String::from_utf8(data).unwrap();
+        Self(username)
     }
 }
